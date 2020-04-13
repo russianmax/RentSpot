e5223f3e3a12b95e8f20123002a0bc14fe70aec1
Matthew Farrelly <matthew.farrelly68@mail.dcu.ie>
2020-04-13 13:37:07 +0100

    setting.py

--- src/personal_portfolio/settings.py
+++ src/personal_portfolio/settings.py
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'projects',
     'blog',
-    'users',
+    'users.apps.UsersConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
